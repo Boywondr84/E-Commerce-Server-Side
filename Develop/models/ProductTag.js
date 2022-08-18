ae474b98265ec1837f0bc9da3d55acd1dfd90cbd
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-18 01:07:49 -0500

    added objects to category, product, tag and product tag files

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -7,6 +7,22 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references product model ID
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references tag model ID
+    }
   },
   {
     sequelize,
