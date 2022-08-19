65b76ead0eb081d54bb21e7758f5b13abccb1ccb
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-19 00:02:20 -0500

    added put routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -49,7 +49,24 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbCatData => {
+      if (!dbCatData[0]) {
+        res.status(404).json({ message: "No category matched that ID" });
+        return;
+      }
+      res.json(dbCatData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
