6dc09e19422df21d6f6071b98269437aa208faa0
Ryan Kahlbaugh <gitworkcoding22@gmail.com>
2022-08-22 22:01:23 -0500

    get/post/put/delete for tags working

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -61,7 +61,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new tag
   Tag.create({
-    tag_id: req.body.tag_id,
+    // tag_id: req.body.tag_id,
     tag_name: req.body.tag_name
   })
     .then(dbTagData => res.json(dbTagData))
